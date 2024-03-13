5b1a5bd86155fa338aea72119c87b70f19df07cb
JamieIsMe <120991704+JamieIsMe@users.noreply.github.com>
2024-03-13 18:55:57 +0000

    New Constructor
    -Constructor that uses a list with all information

--- src/com/project/Loaner.java
+++ src/com/project/Loaner.java
@@ -1,24 +1,28 @@
 package com.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Loaner {
+	String loanID;
 	String gender;
-	String married;
-	int dependants;
+	boolean married;
+	String dependants;
 	String education;
 	boolean selfEmployed;
 	int applicantIncome;
-	int coapplicantIncome;
-	int loanAmount;
-	int loanTerm;
+	Float coapplicantIncome;
+	Float loanAmount;
+	Float loanTerm;
 	boolean creditHistory;
 	String propertyArea;
 	boolean loanStatus;
 	
+	List<String> line = new ArrayList<>();
 	
-	
-	public Loaner(String gender, String married, int dependants, String education,
-			boolean selfEmployed, int applicantIncome, int coapplicantIncome,
-			int loanAmount, int loanTerm, boolean creditHistory,
+	public Loaner(String gender, boolean married, String dependants, String education,
+			boolean selfEmployed, int applicantIncome, Float coapplicantIncome,
+			Float loanAmount, Float loanTerm, boolean creditHistory,
 			String propertyArea, boolean loanStatus) {
 		this.gender = gender;
 		this.married = married;
@@ -34,5 +38,44 @@ public class Loaner {
 		this.loanStatus = loanStatus;
 		
 		
+	}
+	public Loaner(List<String> test) {
+		this.loanID = test.get(0);
+		try {
+			this.gender = test.get(1);
+		} catch(Exception e){
+			this.gender = "Male";
+		}
+		this.married = test.get(2).contains("Yes");
+		
+		try {
+			this.dependants = test.get(3);
+		} catch(Exception e){
+			this.dependants = "0";
+		}
+		this.education = test.get(4);
+		
+		try {
+			this.selfEmployed = test.get(5).contains("Yes");
+		} catch(Exception e){
+			this.selfEmployed = false;
+		}
+		
+		this.applicantIncome = Integer.valueOf(test.get(6));
+		this.coapplicantIncome = Float.valueOf(test.get(7));
+		this.loanAmount =Float.valueOf(test.get(8));
+		try {
+			this.loanTerm = Float.valueOf(test.get(9));
+		} catch(Exception e){
+			this.loanTerm = (float)360.0;
+		}
+		this.creditHistory = test.get(10) == "1.0";
+		this.propertyArea = test.get(11);
+		this.loanStatus = test.get(12)=="Y";
+
+	}
+	
+	public  void showData() {
+		System.out.println(loanID + " " +gender +" " + married + " " + dependants);
 	}
 }
